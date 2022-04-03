8cd75e30e2bda13d1b60eb3cb63e0196574160dc
cangumeli <can.guemeli@tum.de>
2022-04-03 22:04:18 +0200

    Add mobile lazy-load

--- static/js/index.js
+++ static/js/index.js
@@ -120,6 +120,7 @@ window.onload = () => {
     /*document.getElementById('shortVideo').autoplay = false;
     document.getElementById('shortVideo').loop = false;
     document.getElementById('shortVideo').controls = false;*/
+    document.getElementById('viewer3DMobile').src = "static/meshes/mesh_sofa.glb";
     document.getElementById('demoDesktop').hidden = true;
     document.getElementById('demoMobile').hidden = false;
     document.getElementById('userMessage').innerHTML = "The interactive mesh viewer should appear at the bottom";
